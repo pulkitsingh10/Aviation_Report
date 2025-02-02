202869b44313c75221b211e5ff9c7574af7124a0
Pulkit <singhpulkit79@gmail.com>
2025-02-02 16:58:44 +0530

    updated resources

--- pages/2_🚦_Traffic_News.py
+++ pages/2_🚦_Traffic_News.py
@@ -1,29 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-st.image("/Users/pulkit/Downloads/india-airports-map.png")
+st.image("assets/india-airports-map.png")
 
 st.header("Total Passengers Movements")
 
-df1=pd.read_excel("/Users/pulkit/Downloads/Mar2k24Annex3 (1).xlsx",
-                 sheet_name="Table 4",
-                 engine="openpyxl",
-                 skiprows=2
-    )
+df1 = pd.read_excel("assets/Mar2k24Annex3 (1).xlsx",
+                    sheet_name="Table 4",
+                    engine="openpyxl",
+                    skiprows=2
+                    )
 
 st.dataframe(df1)
 
 st.header("Total Aircrafts Movements")
 
-df2=pd.read_excel("/Users/pulkit/Downloads/Mar2k24Annex2 2.xlsx",
-                 sheet_name="Table 4",
-                 engine="openpyxl",
-                 skiprows=2
-    )
+df2 = pd.read_excel("assets/Mar2k24Annex2 2.xlsx",
+                    sheet_name="Table 4",
+                    engine="openpyxl",
+                    skiprows=2
+                    )
 
 st.dataframe(df2)
-
-
-
-
-
--- pages/4_📈_Profitability.py
+++ pages/4_📈_Profitability.py
@@ -1,16 +1,16 @@
 import streamlit as st
-import pandas as pd 
-import plotly.express as px
+import pandas as pd
+
 
 st.title("Profit Table")
 st.subheader("Upload your excel file")
 
-uploaded_file=st.file_uploader('Choose a XLSX file', type='xlsx')
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
 if uploaded_file:
     st.markdown('---')
-    df=pd.read_excel(uploaded_file,
-                     #engine='openpyxl',
-                     skiprows=6,
-                     nrows=1000
-    )
+    df = pd.read_excel(uploaded_file,
+                       # engine='openpyxl',
+                       skiprows=6,
+                       nrows=1000
+                       )
     st.dataframe(df)
